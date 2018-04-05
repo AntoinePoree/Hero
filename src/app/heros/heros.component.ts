b2d96fff2df71df4b908a90400303f67baf56db3
Martins Alexandre <xeos28@gmail.com>
2018-04-05 21:00:49 +0200

    hero oki

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -12,25 +12,30 @@ export class HerosComponent implements OnInit {
   title = 'app';
   results = '';
 
+  heroName;
+  intelligence;
+  speed;
+  img;
+  durability;
+  power;
+  combat;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json').subscribe
-      (data => {
 
-      });
+    let idHeros = 1;
 
-    interface UserResponse {
-      id: number;
-      name: string;
-      powerstats: number;
-      durability: number;
-    }
-    this.http.get<UserResponse>('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/1.json').subscribe((data: any) => {
-      console.log("id: " + data.id);
-      console.log("name: " + data.name);
-      console.log("durability: " + data.powerstats.durability);
+    this.http.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/' + idHeros + '.json').subscribe((data: any) => {
+
+      this.heroName = data.name;
+      this.intelligence = data.powerstats.intelligence;
+      this.speed = data.powerstats.speed;
+      this.durability = data.powerstats.durability;
+      this.power = data.powerstats.power;
+      this.combat = data.powerstats.combat;
+      this.img = data.images.xs;
+
     });
   }
-
 }

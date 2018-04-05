e5dd5fe8448a226a024f21f97bb62fb6c23edb2c
Martins Alexandre <xeos28@gmail.com>
2018-04-05 21:11:44 +0200

    fuuuuuuuuusssssiiiiiiiioooonnnnn her

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,35 +8,74 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
-
 export class HerosComponent implements OnInit {
-
-  title = 'app';
-  results = '';
-
-  heroName;
-  intelligence;
-  speed;
-  img;
   durability;
-  power;
-  combat;
+  idHero;
+  imageHlg;
+  imageHmd;
+  imageHsm;
+  imageHxs;
+  powerH;
+  speedH;
+  strengthH;
+  intelligenceH;
+  nameH;
 
-  constructor(private http: HttpClient) { }
+  durability2;
+  idHero2;
+  imageHlg2;
+  imageHmd2;
+  imageHsm2;
+  imageHxs2;
+  powerH2;
+  speedH2;
+  strengthH2;
+  intelligenceH2;
+  nameH2;
+
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit(): void {
+    let id = 0;
+    let id2 = 0;
+    function getId(n) {
+      id = n;
+      return id;
+    }
+    getId(729);
+    function getIdP2(n) {
+      id2 = n;
+      return id2;
+    }
+    getIdP2(1);
+    this.http.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/' + id + '.json').subscribe((data: any) => {
+      this.idHero = parseInt(data.id, 10);
+      this.nameH = data.name;
+      this.intelligenceH = parseInt(data.powerstats.intelligence, 10);
+      this.strengthH = parseInt(data.powerstats.strength, 10);
+      this.speedH = parseInt(data.powerstats.speed, 10);
+      this.powerH = parseInt(data.powerstats.power, 10);
+      this.imageHxs = data.images.xs;
+      this.imageHsm = data.images.sm;
+      this.imageHmd = data.images.md;
+      this.imageHlg = data.images.lg;
+      this.durability = parseInt(data.powerstats.durability, 10);
 
-    let idHeros = 1;
-
-    this.http.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/' + idHeros + '.json').subscribe((data: any) => {
-
-      this.heroName = data.name;
-      this.intelligence = data.powerstats.intelligence;
-      this.speed = data.powerstats.speed;
-      this.durability = data.powerstats.durability;
-      this.power = data.powerstats.power;
-      this.combat = data.powerstats.combat;
-      this.img = data.images.xs;
+    });
+    this.http.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/' + id2 + '.json').subscribe((data: any) => {
+      this.idHero2 = parseInt(data.id, 10);
+      this.nameH2 = data.name;
+      this.intelligenceH2 = parseInt(data.powerstats.intelligence, 10);
+      this.strengthH2 = parseInt(data.powerstats.strength, 10);
+      this.speedH2 = parseInt(data.powerstats.speed, 10);
+      this.powerH2 = parseInt(data.powerstats.power, 10);
+      this.imageHxs2 = data.images.xs;
+      this.imageHsm2 = data.images.sm;
+      this.imageHmd2 = data.images.md;
+      this.imageHlg2 = data.images.lg;
+      this.durability2 = parseInt(data.powerstats.durability, 10);
 
     });
   }
